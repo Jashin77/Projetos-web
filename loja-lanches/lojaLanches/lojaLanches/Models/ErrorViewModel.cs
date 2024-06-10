ebc28005a516e6f1a73f1c11049f3f5b1eb30195
Braian Dos Santos Da Silva <122707041+Jashin77@users.noreply.github.com>
2024-06-10 17:48:57 -0300

    criação

---
+++ loja-lanches/lojaLanches/lojaLanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lojaLanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
