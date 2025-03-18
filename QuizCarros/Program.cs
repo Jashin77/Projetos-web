2166533583a53a6506e69bec14b4da009bd606bd
Braian Dos Santos Da Silva <122707041+Jashin77@users.noreply.github.com>
2025-03-18 19:09:57 -0300

    correção de bug

--- QuizCarros/Program.cs
+++ QuizCarros/Program.cs
@@ -5,12 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
--- QuizCarros/wwwroot/js/site.js
+++ QuizCarros/wwwroot/js/site.js
@@ -2,35 +2,28 @@
 const form = document.querySelector('form');
 const respostaInput = document.getElementById('resposta-input');
 
-// Verifica se os elementos foram encontrados
 console.log("Botões encontrados:", buttons);
 console.log("Formulário encontrado:", form);
 console.log("Input encontrado:", respostaInput);
 
-// Adiciona o evento de clique
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const respostaSelecionada = button.dataset.resposta;
 
-        // Pega a resposta correta do body
         const respostaCorreta = parseInt(document.body.getAttribute('data-resposta-correta'));
         console.log("Resposta correta:", respostaCorreta);
         console.log("Resposta selecionada:", respostaSelecionada);
 
-        // Passa a resposta pro input hidden
         respostaInput.value = respostaSelecionada;
 
-        // Muda a cor do botão
         if (parseInt(respostaSelecionada) === respostaCorreta) {
-            button.classList.add('correct'); // Verde
+            button.classList.add('correct');
         } else {
-            button.classList.add('incorrect'); // Vermelho
+            button.classList.add('incorrect'); 
         }
 
-        // Desabilita todos os botões
         buttons.forEach(btn => btn.disabled = true);
 
-        // Envia o formulário após 1 segundo
         setTimeout(() => {
             form.submit();
         }, 1000);
