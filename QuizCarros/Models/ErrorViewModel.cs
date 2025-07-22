748c615dd85b24a39d3f5fc21c4c0e61ffd07017
Braian Dos Santos Da Silva <122707041+Jashin77@users.noreply.github.com>
2025-07-22 12:05:15 -0300

    Delete QuizCarros directory

--- QuizCarros/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuizCarros.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
